ee0393aa4c453a72dfbe6e60300f03d1dd72e691
Lukas Hauzenberger <lukas.hauzenberger@gmail.com>
2021-08-10 06:14:18 +0000

    parallelize get_img_stats

--- get_img_stats.py
+++ get_img_stats.py
@@ -2,19 +2,25 @@ from pathlib import Path
 from PIL import Image
 import numpy as np
 import dill as pickle
+from joblib import Parallel, delayed
+from multiprocessing import cpu_count
+
+
+def get_stats(filepath):
+    ar = np.asarray(Image.open(filepath), dtype=np.float32)
+    ar /= 255
+    return np.array([ar.sum(), np.square(ar).sum(), ar.size])    
 
 
 print("Enter image directory:")
 input_dir = input()
 
-running_stats = np.zeros((3,), dtype=np.float32)
-for f in Path(input_dir).rglob("*.jpg"):
-    ar = np.asarray(Image.open(f), dtype=np.float32)
-    ar /= 255
-    running_stats += np.array([ar.sum(), np.square(ar).sum(), ar.size])
+n_cpu = cpu_count()
+res = Parallel(n_jobs=max(n_cpu-1, 1))(delayed(get_stats)(f) for f in Path(input_dir).rglob("*.jpg"))
+stats = np.stack(res).sum(0)
     
-mean = running_stats[0] / running_stats[2]
-std = np.sqrt(running_stats[1] / running_stats[2] - mean**2)
+mean = stats[0] / stats[2]
+std = np.sqrt(stats[1] / stats[2] - mean**2)
 
 with open("data/image_stats.pkl", "wb") as f:
     pickle.dump([float(mean), float(std)], f)
